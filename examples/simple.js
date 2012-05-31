0c571233f9f30916c629f4f3564a20576e259a1c
Ric Allinson <capecodehq@yahoo.co.uk>
2012-05-31 12:43:45 -0700

    Added comments and ran jslint.

--- examples/simple.js
+++ examples/simple.js
@@ -21,6 +21,8 @@
 //    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 //    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+"use strict";
+
 var config = require('../lib').create([
     {
         "env": {
